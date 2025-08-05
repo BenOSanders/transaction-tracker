f2c427d23807dd302c7da141fc21a0abda23d46d
Ben Sanders <8213807+BenOSanders@users.noreply.github.com>
2025-08-04 20:32:25 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+const app = express();
+app.use(express.json());
+
+const configuration = new Configuration({
+	basePath: PlaidEnvironments[process.env.PLAID_ENV],
+	baseOptions: {
+		headers: {
+			'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+			'PLAID-SECRET': process.env.PLAID_SECRET,
+		}
+	},
+});
+
+const client = new PlaidApi(configuration);
+
+// todo: store, encrypted, in database
+let access_token = null;
+
+// Create link token
+
+// Exchange public token for access token
+
+// Pull transaction data
+
+
