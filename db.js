e49e46599ccdf008350b91e55756c5be3186ae38
Ben Sanders <8213807+BenOSanders@users.noreply.github.com>
2025-08-31 21:18:30 -0400

    adding db

---
+++ db.js
@@ -0,0 +1,34 @@
+import Database from 'better-sqlite3';
+
+const db = Database('bank.db')
+
+db.exec(`
+CREATE TABLE IF NOT EXISTS transactions(
+    transaction_id TEXT PRIMARY KEY,
+    account_id TEXT,
+    name TEXT,
+    amount REAL,
+    date TEXT,
+    category TEXT,
+    category_confidence TEXT,
+    payment_channel TEXT,
+    merchent_name TEXT,
+    merchent_entity_id TEXT,
+    website TEXT,
+    merchent_logo TEXT,
+    address TEXT,
+    city TEXT,
+    state TEXT,
+    zipcode REAL,
+);
+
+CREATE TABLE IF NOT EXISTS sync_state(
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    cursor TEXT
+);
+    `)
+
+
+db.prepare("INSERT OR IGNORE INTO sync_state (id, cursor) VALUES (1, NULL)").run();
+
+export default db;
